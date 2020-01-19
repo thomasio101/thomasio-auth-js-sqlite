de0a7cdd4b9a0d3ee062b01cbcb1810a1e8075e3
Thomas Egbert Duursma <thomasduursma@outlook.com>
2020-01-19 21:48:19 +0100

    Update index.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,58 @@
-import { open } from 'sqlite';
-import { UserAuthenticator } from 'thomasio-auth-js-common/lib/server';
+import { Database, open } from 'sqlite';
+import { IDatabaseInterface, ISession, PasswordVerifier } from 'thomasio-auth-js-common/lib/server';
 
-export function getUserAuthenticator<T>(dbFileName: string): UserAuthenticator<T> {
-	const dbPromise = open(dbFileName);
+export type UserFetcher<I, P> = (
+	username: string,
+	dbPromise: Promise<Database>,
+) => Promise<{ storedPassword: P; identity: I } | null>;
+
+export type SessionCreator<I> = (identity: I) => Promise<ISession<I>>;
+
+export class SqliteDatabaseInterface<I, P> implements IDatabaseInterface<I> {
+	private dbPromise: Promise<Database>;
+	private userFetcher: UserFetcher<I, P>;
+	private passwordVerifier: PasswordVerifier<P>;
+	private sessionCreator: SessionCreator<I>;
+
+	// TODO: Replace identity in userFetcher with a function which fetches the identity.
+	constructor(
+		dbFileName: string,
+		userFetcher: UserFetcher<I, P>,
+		passwordVerifier: PasswordVerifier<P>,
+		sessionCreator: SessionCreator<I>,
+	) {
+		this.dbPromise = open(dbFileName);
+		this.userFetcher = userFetcher;
+		this.passwordVerifier = passwordVerifier;
+		this.sessionCreator = sessionCreator;
+	}
+
+	public async userAuthenticator(
+		username: string,
+		password: string,
+	): Promise<{ valid: true; session: ISession<I> } | { valid: false }> {
+		const userFetcherResult = await this.userFetcher(username, this.dbPromise);
+
+		if (userFetcherResult !== null) {
+			if (await this.passwordVerifier(userFetcherResult.storedPassword, password)) {
+				return {
+					session: await this.sessionCreator(userFetcherResult.identity),
+					valid: true,
+				};
+			} else {
+				return {
+					valid: false,
+				};
+			}
+		} else {
+			return {
+				valid: false,
+			};
+		}
+	}
+
+	public async sessionAuthenticator(session: ISession<I>) {
+		// TODO: Implement SqliteDatabaseInterface.sessionAuthenticator
+		return false;
+	}
 }
