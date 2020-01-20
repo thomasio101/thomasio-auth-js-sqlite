5607a839c7d079c41b71e5792320172938c4c9e8
Thomas Egbert Duursma <thomasduursma@outlook.com>
2020-01-20 16:34:09 +0100

    Replace PasswordVerifier with Verifier.
    This change has been made due to the update of thomasio-auth-js-common to 1.3.0.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Database, open } from 'sqlite';
-import { IDatabaseInterface, ISession, PasswordVerifier } from 'thomasio-auth-js-common/lib/server';
+import { IDatabaseInterface, ISession, Verifier } from 'thomasio-auth-js-common/lib/server';
 
 export type UserFetcher<I, P> = (
 	username: string,
@@ -11,14 +11,14 @@ export type SessionCreator<I> = (identity: I) => Promise<ISession<I>>;
 export class SqliteDatabaseInterface<I, P> implements IDatabaseInterface<I> {
 	private dbPromise: Promise<Database>;
 	private userFetcher: UserFetcher<I, P>;
-	private passwordVerifier: PasswordVerifier<P>;
+	private passwordVerifier: Verifier<P>;
 	private sessionCreator: SessionCreator<I>;
 
 	// TODO: Replace identity in userFetcher with a function which fetches the identity.
 	constructor(
 		dbFileName: string,
 		userFetcher: UserFetcher<I, P>,
-		passwordVerifier: PasswordVerifier<P>,
+		passwordVerifier: Verifier<P>,
 		sessionCreator: SessionCreator<I>,
 	) {
 		this.dbPromise = open(dbFileName);
